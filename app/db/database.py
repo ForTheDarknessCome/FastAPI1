d8b32d17c567b14408248e15d95ea58d61e48ead
Ilya Rodman <i.nuyanzin@yandex.ru>
2025-01-23 21:55:11 +0400

    v0.1

---
+++ app/db/database.py
@@ -0,0 +1,23 @@
+from typing import Annotated
+from sqlalchemy.orm import DeclarativeBase, declared_attr, mapped_column
+from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
+from config import settings
+
+DATABASE_URL = settings.get_db_url()
+
+engine = create_async_engine(url=DATABASE_URL)
+
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+# Кастомные аннотации
+int_pk = Annotated[int, mapped_column(primary_key=True)]
+str_uniq = Annotated[str, mapped_column(unique=True, nullable=False)]
+str_null_true = Annotated[str, mapped_column(nullable=True)]
+
+
+class Base(AsyncAttrs, DeclarativeBase):
+    __abstract__ = True
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return f"{cls.__name__.lower()}s"
